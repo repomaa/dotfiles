25800d90fc89bfa1420746b108e77a49225dcb94
Joakim Repomaa <mail@j.repomaa.com>
2021-11-01 12:26:55 +0100

    update dotfiles

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -1,3 +1,9 @@
+local cmd = vim.cmd
 local opt = vim.opt
 
 opt.number = true
+opt.ts = 4
+opt.sts = 4
+opt.sw = 4
+
+cmd('autocmd FileType crystal,ecrystal,ruby,eruby setlocal ts=2 sts=2 sw=2')
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -6,7 +6,6 @@ local tbl_extend = vim.tbl_extend
 local tbl_filter = vim.tbl_filter
 local tbl_contains = vim.tbl_contains
 local list_extend = vim.list_extend
-local set_keymap = vim.api.nvim_set_keymap
 
 ensure_packer = function()
 	local installpath = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -22,9 +21,11 @@ packer = require('packer')
 packer.startup(function()
 	local js_fts = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
 	local ruby_fts = { 'ruby', 'eruby' }
+	local crystal_fts = { 'crystal', 'ecrystal' }
 	local tabnine_fts = {}
 	list_extend(tabnine_fts, js_fts)
 	list_extend(tabnine_fts, ruby_fts)
+	list_extend(tabnine_fts, crystal_fts)
 
 	local coc = function(options)
 		local merged = tbl_extend('keep', options, {
@@ -54,10 +55,56 @@ packer.startup(function()
 		end
 
 	}
+	use { 'vim-crystal/vim-crystal',
+		setup = function ()
+			vim.cmd('autocmd BufNewFile,BufReadPost *.cr setlocal filetype=crystal')
+			vim.cmd('autocmd BufNewFile,BufReadPost Projectfile setlocal filetype=crystal')
+			vim.cmd('autocmd BufNewFile,BufReadPost *.ecr setlocal filetype=ecrystal')
+		end,
+		ft = crystal_fts
+	}
+	use { 'vim-scripts/DrawIt',
+		keys = { '<Leader>di' }
+	}
+	use { 'tpope/vim-fugitive',
+		fn = 'fugitive#',
+		cmd = { 'Gwrite', 'Gdiff', 'Gdiffsplit', 'Gread', 'Git', 'G', 'Git!', 'G!' }
+	}
+
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {
+			{'nvim-lua/plenary.nvim'},
+			{'nvim-telescope/telescope-fzf-native.nvim'}
+		},
+		module = 'telescope',
+		setup = function ()
+			local map = vim.api.nvim_set_keymap
+			local telemap = function(lhs, func, args)
+				local rhs = [[<cmd>lua require('telescope.builtin').]] .. func .. '(' .. args .. ')<cr>'
+				map('n', lhs, rhs, { noremap = true })
+			end
+
+			telemap('<Leader>f', 'find_files', '{ hidden = true }')
+			telemap('<Leader>g', 'live_grep', '{ hidden = true }')
+			telemap('<Leader>b', 'buffers', '')
+			telemap('<Leader>h', 'help_tags', '')
+			telemap('<Leader>n', 'file_browser', '{ hidden = true }')
+		end
+	}
+
+	use {
+		'jparise/vim-graphql',
+		setup = function ()
+			vim.cmd('autocmd BufNewFile,BufReadPost *.gql,*.graphql setlocal filetype=graphql')
+		end,
+	}
 
 	coc { 'neoclide/coc-tsserver', ft = js_fts }
 	coc { 'neoclide/coc-prettier', ft = js_fts }
 	coc { 'neoclide/coc-solargraph', ft = ruby_fts }
 	coc { 'neoclide/coc-yaml', ft = { 'yaml' } }
 	coc { 'neoclide/coc-tabnine', ft = tabnine_fts }
+	coc { 'neoclide/coc-json', ft = { 'json' } }
+	coc { 'iamcco/coc-diagnostic', ft = { 'sh' } }
 end)
