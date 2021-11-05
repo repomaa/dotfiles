c7b930f1392d900b3e05102aa956e40fdee29bf6
Joakim Repomaa <joakim.repomaa@mocoapp.com>
2021-11-06 00:53:11 +0200

    update vim config

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -1,9 +1,18 @@
 local cmd = vim.cmd
 local opt = vim.opt
+local fn = vim.fn
 
 opt.number = true
 opt.ts = 4
 opt.sts = 4
 opt.sw = 4
 
-cmd('autocmd FileType crystal,ecrystal,ruby,eruby setlocal ts=2 sts=2 sw=2')
+local undodir = fn.stdpath('cache')..'/undo'
+fn.mkdir(undodir, 'p')
+
+opt.undofile = true
+opt.undodir = undodir
+
+opt.hidden = true
+
+cmd('autocmd FileType crystal,ecrystal,ruby,eruby,javascriptreact,typescriptreact setlocal ts=2 sts=2 sw=2')
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -7,6 +7,8 @@ local tbl_filter = vim.tbl_filter
 local tbl_contains = vim.tbl_contains
 local list_extend = vim.list_extend
 
+local plugins = {}
+
 ensure_packer = function()
 	local installpath = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 	if fn.empty(fn.glob(installpath)) > 0 then
@@ -19,6 +21,9 @@ ensure_packer()
 packer = require('packer')
 
 packer.startup(function()
+	use { 'svermeulen/vimpeccable' }
+	vimp = require('vimp')
+
 	local js_fts = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
 	local ruby_fts = { 'ruby', 'eruby' }
 	local crystal_fts = { 'crystal', 'ecrystal' }
@@ -29,16 +34,48 @@ packer.startup(function()
 
 	local coc = function(options)
 		local merged = tbl_extend('keep', options, {
-			requires = { 'neoclide/coc.nvim', run = 'yarn install --frozen-lockfile' },
+			requires = { 'neoclide/coc.nvim',
+				run = 'yarn install --frozen-lockfile',
+				config = function()
+					local cocmap = function(lhs, func)
+						local rhs = '<Plug>(coc-' .. func .. ')'
+						vimp.nmap({ 'silent' }, lhs, rhs)
+					end
+
+					cocmap('[g', 'diagnostic-prev')
+					cocmap(']g', 'diagnostic-next')
+					cocmap('gd', 'definition')
+					cocmap('gy', 'type-definition')
+					cocmap('gi', 'implementation')
+					cocmap('gr', 'references')
+
+					vimp.nnoremap('K', function()
+						local word = vim.fn.expand('<cword>')
+						local ft = vim.bo.filetype
+						if (ft == 'vim' or ft == 'help')
+						then
+							vim.cmd('h ' .. word)
+						elseif (vim.fn['coc#rpc#ready']())
+						then
+							vim.fn['CocActionAsync']('doHover')
+						else
+							vim.cmd('!man ' .. word)
+						end
+					end)
+
+					vim.cmd([[autocmd CursorHold * silent call CocActionAsync('highlight')]])
+				end
+			},
 			run = 'yarn install --frozen-lockfile',
 			keys = { '<Plug>(coc-' },
-			fn = 'coc#'
+			fn = 'coc#',
 		})
 
 		use(merged)
 	end
 
 	use 'wbthomason/packer.nvim'
+
 	use { 'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
 		config = function()
@@ -48,6 +85,9 @@ packer.startup(function()
 			}
 		end
 	}
+
+	use { 'romgrk/nvim-treesitter-context' }
+
 	use { 'chriskempson/base16-vim',
 		config = function()
 			vim.g['base16colorspace'] = 256
@@ -55,6 +95,7 @@ packer.startup(function()
 		end
 
 	}
+
 	use { 'vim-crystal/vim-crystal',
 		setup = function ()
 			vim.cmd('autocmd BufNewFile,BufReadPost *.cr setlocal filetype=crystal')
@@ -63,48 +104,111 @@ packer.startup(function()
 		end,
 		ft = crystal_fts
 	}
+
 	use { 'vim-scripts/DrawIt',
 		keys = { '<Leader>di' }
 	}
+
 	use { 'tpope/vim-fugitive',
+		requires = { 'tpope/vim-rhubarb' },
 		fn = 'fugitive#',
-		cmd = { 'Gwrite', 'Gdiff', 'Gdiffsplit', 'Gread', 'Git', 'G', 'Git!', 'G!' }
+		cmd = {
+			'Gread',
+			'Gwrite',
+			'Gdiff',
+			'Gdiffsplit',
+			'Gvdiffsplit',
+			'GMove',
+			'GDelete',
+			'GBrowse',
+			'GRename',
+			'Git',
+			'G',
+			'Git!',
+			'G!'
+		}
 	}
 
-	use {
-		'nvim-telescope/telescope.nvim',
+	use { 'nvim-telescope/telescope.nvim',
 		requires = {
 			{'nvim-lua/plenary.nvim'},
 			{'nvim-telescope/telescope-fzf-native.nvim'}
 		},
-		module = 'telescope',
-		setup = function ()
-			local map = vim.api.nvim_set_keymap
-			local telemap = function(lhs, func, args)
-				local rhs = [[<cmd>lua require('telescope.builtin').]] .. func .. '(' .. args .. ')<cr>'
-				map('n', lhs, rhs, { noremap = true })
+		config = function ()
+			local telemap = function(lhs, func, ...)
+				local rhs = require('telescope.builtin')[func]
+				local options = ...
+				vimp.nnoremap(lhs, function ()
+					if (options) then rhs(options) else rhs() end
+				end)
 			end
 
-			telemap('<Leader>f', 'find_files', '{ hidden = true }')
-			telemap('<Leader>g', 'live_grep', '{ hidden = true }')
-			telemap('<Leader>b', 'buffers', '')
-			telemap('<Leader>h', 'help_tags', '')
-			telemap('<Leader>n', 'file_browser', '{ hidden = true }')
+			telemap('<Leader>f', 'find_files', { hidden = true })
+			telemap('<Leader>r', 'live_grep', { hidden = true })
+			telemap('<Leader>b', 'buffers')
+			telemap('<Leader>h', 'help_tags')
+			telemap('<Leader>n', 'file_browser', { hidden = true })
+			telemap('<Leader>o', 'oldfiles', { hidden = true })
 		end
 	}
 
-	use {
-		'jparise/vim-graphql',
+	use { 'nvim-telescope/telescope-frecency.nvim',
+		requires = { 'tami5/sqlite.lua' },
+		config = function()
+			local telescope = require('telescope')
+			telescope.load_extension('frecency')
+			vimp.nnoremap('<Leader>m', telescope.extensions.frecency.frecency)
+		end
+	}
+
+	use { 'jparise/vim-graphql',
 		setup = function ()
 			vim.cmd('autocmd BufNewFile,BufReadPost *.gql,*.graphql setlocal filetype=graphql')
 		end,
 	}
 
+	use { 'pwntester/octo.nvim',
+		config = function()
+			require('octo').setup()
+		end
+	}
+
+	use { 'fannheyward/telescope-coc.nvim',
+		config = function()
+			local telescope = require('telescope')
+			telescope.load_extension('coc')
+
+			vimp.nnoremap('<Leader>ds', function ()
+				telescope.extensions.coc.document_symbols({})
+			end)
+
+			vimp.nnoremap('<Leader>ws', function ()
+				telescope.extensions.coc.workspace_symbols({})
+			end)
+		end
+	}
+
+	use { 'lewis6991/gitsigns.nvim',
+		requires = { 'nvim-lua/plenary.nvim' },
+		config = function ()
+			require('gitsigns').setup()
+		end
+	}
+
+	use { 'windwp/windline.nvim',
+		config = function()
+			require('wlsample.airline')
+		end
+	}
+
 	coc { 'neoclide/coc-tsserver', ft = js_fts }
 	coc { 'neoclide/coc-prettier', ft = js_fts }
+	coc { 'neoclide/coc-eslint', ft = js_fts }
 	coc { 'neoclide/coc-solargraph', ft = ruby_fts }
 	coc { 'neoclide/coc-yaml', ft = { 'yaml' } }
 	coc { 'neoclide/coc-tabnine', ft = tabnine_fts }
 	coc { 'neoclide/coc-json', ft = { 'json' } }
 	coc { 'iamcco/coc-diagnostic', ft = { 'sh' } }
 end)
+
+return plugins
